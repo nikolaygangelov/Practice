7180a15ca183fe026b354069ab2664308321c98d
nikolaygangelov <nikolay.g.angelov@gmail.com>
2024-11-06 20:57:07 +0200

    Add project files.

---
+++ Accedia1/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Binary_Sort
+{
+	public class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] arr = new int[] { 1, 27, 16, 4 };
+
+			Array.Sort(arr, (a,b) =>
+			{
+				int lastFourBitsA = a & 0b1111;
+				int lastFourBitsB = b & 0b1111;
+
+				return lastFourBitsA.CompareTo(lastFourBitsB);
+			});
+
+            Console.WriteLine(String.Join(", ", arr));
+        }
+	}
+}
