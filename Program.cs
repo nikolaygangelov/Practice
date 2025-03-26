b6aff96487dc21ad22a992417b8b7ab61cc8fc0f
Nikolay Angelov <108483720+nikolaygangelov@users.noreply.github.com>
2025-03-26 21:48:06 +0200

    renaming, deleting and commenting

--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
-﻿namespace Binary_Sort
+﻿namespace Practice
 {
 	public class Program
 	{
 		static void Main(string[] args)
 		{
-			//Sort the array using 4-bit unsigned binary values of array numbers
+			// Sort the array using 4-bit unsigned binary values of array numbers
 			int[] arr = new int[] { 1, 27, 16, 4 };
 
 			Array.Sort(arr, (a,b) =>
@@ -16,6 +16,28 @@
 			});
 
             Console.WriteLine(String.Join(", ", arr));
+
+			// Practice If-Else
+			// looping through different scenarios about Fizz and Buzz
+			for(int i = 1;i<=n; i++)
+			{
+                if(i % 3 == 0 && i % 5 != 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if(i % 5 == 0 && i % 3 != 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if(i % 5 == 0 && i % 3 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
         }
 	}
 }
